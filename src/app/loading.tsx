a9cfac35f535db05763f7599f6b33b3dc3ff995b
Samuel Gularte <31748265+SamGularte@users.noreply.github.com>
2024-01-25 22:38:14 -0300

    loading component

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "./loading.module.css";
 
 const Loader = () => {
-  return <div>loading...</div>;
+  return (
+    <div className={styles.LoadingContainer}>
+      <p>loading...</p>
+    </div>
+  );
 };
 
 export default Loader;
