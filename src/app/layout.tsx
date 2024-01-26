b131226104d439a88e16931f04716ecafc1f8d79
Samuel Gularte <31748265+SamGularte@users.noreply.github.com>
2024-01-25 23:09:29 -0300

    metadata updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,16 @@ export const metadata: Metadata = {
   title: "Difras News",
   description:
     "Difras News, sua fonte confiável e ágil de informações, trazendo notícias relevantes e atualizadas para mantê-lo informado sobre os acontecimentos mais importantes do mundo.",
+  icons: {
+    icon: [
+      { url: "", sizes: "32x32" },
+      { url: "", sizes: "16x16" },
+    ],
+    apple: {
+      url: "",
+      sizes: "180x180",
+    },
+  },
   openGraph: {
     title: "Difras News",
     description:
@@ -19,7 +29,14 @@ export const metadata: Metadata = {
     type: "article",
     authors: "SamGularte",
   },
-  keywords: ["noticias", "notícias", "difras", "difras news"],
+  keywords: [
+    "noticias",
+    "notícias",
+    "difras",
+    "difras news",
+    "news",
+    "news about  everything",
+  ],
 };
 
 export default function RootLayout({
